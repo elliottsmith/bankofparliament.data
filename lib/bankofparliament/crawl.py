472c0f7a75f9c6ef1f5d663401a674519b8e93cd
elliott <elliott@elliotts-MacBook-Pro.local>
2020-12-24 02:58:02 +0000

    Skeleton of crawler tool

---
+++ lib/bankofparliament/crawl.py
@@ -0,0 +1,23 @@
+"""
+Module for crawl related tasks
+"""
+# -*- coding: utf-8 -*-
+
+# sys libs
+
+# third party libs
+import pandas
+
+# local libs
+
+class CrawlEntities:
+    """Crawls for more entity info"""
+
+    def __init__(self, entities, logger):
+        """Initialise the crawl class"""
+        self.entities = pandas.read_csv(entities)
+        self.logger = logger
+
+    def execute(self):
+        """"""
+        print(self.entities)
